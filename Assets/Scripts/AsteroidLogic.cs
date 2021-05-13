c6dca045f13944903576cb09d69365365b5fef39
sykes19 <55731130+sykes19@users.noreply.github.com>
2021-05-13 18:04:45 -0500

    Despite what it looks like, literally nothing was accomplished

--- Assets/Scripts/AsteroidLogic.cs
+++ Assets/Scripts/AsteroidLogic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarSpawnerLogic : MonoBehaviour
+public class AsteroidLogic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
